627c14f749adab9b0e01025343690c3c2cff3cc8
kimxons <kitongameshack9@gmail.com>
2023-02-10 10:57:55 +0300

    first commit

---
+++ src/main/java/com/example/insurance_component/InsuranceComponentApplication.java
@@ -0,0 +1,14 @@
+package com.example.insurance_component;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceComponentApplication.class, args);
+		
+	}
+
+}
