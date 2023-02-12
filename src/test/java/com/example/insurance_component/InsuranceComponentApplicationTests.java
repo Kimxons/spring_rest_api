b552964cedcfb5760df1755cba86f406cb372260
kimxons <kitongameshack9@gmail.com>
2023-02-12 10:37:03 +0300

    Made changes on data connection

--- src/test/java/com/example/insurance_component/InsuranceComponentApplicationTests.java
+++ src/test/java/com/example/insurance_component/InsuranceComponentApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.insurance_component;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class InsuranceComponentApplicationTests {
+class DataSourceConfigTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private DataSource dataSource;
 
+    @Test
+    void testConnection() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        assertNotNull(connection, "Connection should not be null");
+        assertTrue(connection.isValid(0), "Connection should be valid");
+        connection.close();
+    }
 }
